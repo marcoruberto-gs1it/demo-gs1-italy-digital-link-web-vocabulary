f4835a80566ca1565f217267c85c28e5618af279
marcoruberto-gs1it <marco.ruberto@indicod-ecr.it>
2026-06-29 17:15:36 +0200

    update info

--- src/app/product-component/product-component.ts
+++ src/app/product-component/product-component.ts
@@ -12,12 +12,12 @@ export class ProductComponent implements OnInit {
   hasBatch = false;
   hasBestBefore = false;
 
-  // Dati reali estratti dalle fonti ufficiali GS1 del prodotto
+  // Dati reali certificati estratti dalle fonti ufficiali GS1 del prodotto [cite: 81, 86]
   product = {
-    nameIT: 'Confettura extra di fragole a ridotto contenuto di zuccheri',
-    functionalName: 'Confettura light',
-    shortDescription: 'Confettura Fragola Light',
-    gtin: '8005360007746', 
+    nameIT: 'Confettura extra di fragole a ridotto contenuto di zuccheri', // [cite: 81, 86]
+    functionalName: 'Confettura light', // [cite: 81]
+    shortDescription: 'Confettura Fragola Light', // [cite: 81]
+    gtin: '8005360007746', // [cite: 81, 86]
     batch: '',
     bestBefore: '',
     bestBeforeFormatted: '',
@@ -28,75 +28,84 @@ export class ProductComponent implements OnInit {
     currency: 'EUR',
     stockStatus: 'InStock',
     stockDisplay: 'Disponibile',
-    gpcCategory: '10000217 - Marmellate/Confetture (Ambiente)',
-    gpcCode: '10000217',
-    targetMarkets: ['ITALIA (380)'],
+    gpcCategory: '10000217 - Marmellate/Confetture (Ambiente)', // [cite: 81, 86]
+    gpcCode: '10000217', // [cite: 81]
+    targetMarkets: ['ITALIA (380)'], // [cite: 81, 86]
     
     // --- DATI DI ETICHETTA E COMPOSIZIONE ---
-    ingredients: 'Fragole, acqua, zucchero, gelificante: pectina da frutta.',
-    fruitContent: '70g per 100g di prodotto finito',
-    fruitPercentage: 70,
-    sugarContent: '20g per 100g', 
-    sugarPercentage: 20, 
+    ingredients: 'Fragole, acqua, zucchero, gelificante: pectina da frutta.', // [cite: 81, 86]
+    fruitContent: '70g per 100g di prodotto finito', // [cite: 86]
+    fruitPercentage: 70, // [cite: 86]
+    sugarContent: '20g per 100g', // [cite: 86]
+    sugarPercentage: 20, // [cite: 86]
     
-    // Allergeni con evidenza delle contaminazioni crociate (MAY_CONTAIN)
-    allergensStatement: 'Prodotto in uno stabilimento che utilizza sedano, latte, soia, frutta a guscio, pesce e molluschi.',
-    allergensList: ['Sedano', 'Pesce', 'Latte', 'Molluschi', 'Soia', 'Frutta a guscio'],
+    // Lista ingredienti strutturata per la vista granulare [cite: 86]
+    ingredientsList: [
+      { name: 'Fragole', percentage: '70%' }, // [cite: 86]
+      { name: 'Acqua', percentage: null }, // [cite: 86]
+      { name: 'Zucchero', percentage: null }, // [cite: 86]
+      { name: 'Gelificante: pectina da frutta', percentage: null } // [cite: 86]
+    ],
+    
+    // Allergeni con evidenza delle contaminazioni crociate (MAY_CONTAIN) [cite: 86]
+    allergensStatement: 'Prodotto in uno stabilimento che utilizza sedano, latte, soia, frutta a guscio, pesce e molluschi.', // [cite: 86]
+    allergensList: ['Sedano', 'Pesce', 'Latte', 'Molluschi', 'Soia', 'Frutta a guscio'], // [cite: 86]
     
     // --- MARKETING & SICUREZZA ---
-    brandName: 'Le Conserve della Nonna',
-    subBrand: 'Light',
-    marketingText: 'Le nostre confetture Light nascono dalla frutta migliore e da ricette semplici, come fatte in casa. Zero edulcoranti e 50% di zuccheri in meno*, per esaltare il gusto autentico della frutta.',
-    features: ['-50% di zuccheri*', 'Zero edulcoranti', '70% di Frutta', 'Fragola Italiana'],
-    safetyWarning: 'CAPSULA DI SICUREZZA: RIFIUTARE SE IL BOTTONE È RIALZATO',
-    storageInstructions: 'Dopo l\'apertura conservare in frigorifero e consumare entro 7 giorni.',
-    storageType: 'Ambiente',
+    brandName: 'Le Conserve della Nonna', // [cite: 81, 86]
+    subBrand: 'Light', // [cite: 86]
+    marketingText: 'Le nostre confetture Light nascono dalla frutta migliore e da ricette semplici, come fatte in casa. Zero edulcoranti e 50% di zuccheri in meno*, per esaltare il gusto autentico della frutta.', // [cite: 86]
+    features: ['-50% di zuccheri*', 'Zero edulcoranti', '70% di Frutta', 'Fragola Italiana'], // [cite: 86]
+    safetyWarning: 'CAPSULA DI SICUREZZA: RIFIUTARE SE IL BOTTONE È RIALZATO', // [cite: 86]
+    storageInstructions: 'Dopo l\'apertura conservare in frigorifero e consumare entro 7 giorni.', // [cite: 81, 86]
+    storageType: 'Ambiente', // [cite: 81, 86]
     
     // --- DATI LOGISTICI COMPLETI ---
-    netWeight: '235',
-    grossWeight: '388',
+    netWeight: '235', // [cite: 81, 86]
+    grossWeight: '388', // [cite: 81, 86]
     dimensions: {
-      height: '127',
-      width: '58',
-      depth: '58',
-      unit: 'mm'
+      height: '127', // [cite: 81, 86]
+      width: '58', // [cite: 81, 86]
+      depth: '58', // [cite: 86]
+      unit: 'mm' // [cite: 81]
     },
     logisticsInfo: {
-      isBaseUnit: true,
-      isConsumerUnit: true,
-      isOrderable: false,
-      isInvoiceable: true
+      isBaseUnit: true, // [cite: 81]
+      isConsumerUnit: true, // [cite: 81]
+      isOrderable: false, // [cite: 81]
+      isInvoiceable: true // [cite: 81]
     },
 
     // --- VALORI NUTRIZIONALI REALI ---
     nutrition: {
-      energyKJ: 383,
-      energyKcal: 91,
-      fat: 0,
-      saturatedFat: 0,
-      carbohydrates: 22,
-      sugars: 20,
+      energyKJ: 383, // [cite: 86]
+      energyKcal: 91, // [cite: 86]
+      fat: 0, // [cite: 86]
+      saturatedFat: 0, // [cite: 86]
+      carbohydrates: 22, // [cite: 86]
+      sugars: 20, // [cite: 86]
       fiber: 0,
-      protein: 0,
-      salt: 0
+      protein: 0, // [cite: 86]
+      salt: 0 // [cite: 86]
     },
 
     // --- PACKAGING E RICICLO (PPWR) ---
     packaging: [
-      { type: 'Vaso/Barattolo', material: 'Vetro Chiaro', code: 'GL 70', recycling: 'Vetro', icon: '🫙' },
-      { type: 'Tappo', material: 'Composito Plastica/Banda Stagnata', code: 'C/FE 91', recycling: 'Metallo', icon: '🥫' }
+      { type: 'Vaso/Barattolo', material: 'Vetro Chiaro', code: 'GL 70', recycling: 'Vetro', icon: '🫙' }, // [cite: 86]
+      { type: 'Tappo', material: 'Composito Plastica/Banda Stagnata', code: 'C/FE 91', recycling: 'Metallo', icon: '🥫' } // [cite: 86]
     ],
     
     // --- PRODUTTORE ---
     brandOwner: {
-      name: 'Gruppo Fini S.p.A. a socio unico',
-      streetAddress: 'Via Confine, 1583',
-      postalCode: '41017',
-      city: 'Ravarino',
-      province: 'MO',
+      name: 'Gruppo Fini S.p.A. a socio unico', // [cite: 81, 86]
+      gln: '8005360000006', // [cite: 81]
+      streetAddress: 'Via Confine, 1583', // [cite: 86]
+      postalCode: '41017', // [cite: 86]
+      city: 'Ravarino', // [cite: 86]
+      province: 'MO', // [cite: 86]
       countryCode: 'IT',
-      telephone: '+39 059 900432',
-      website: 'www.leconservedellanonna.it'
+      telephone: '+39 059 900432', // [cite: 86]
+      website: 'www.leconservedellanonna.it' // [cite: 86]
     }
   };
 
@@ -154,10 +163,28 @@ export class ProductComponent implements OnInit {
         '@type': 'ReferencedFileDetails',
         'referencedFileURL': { '@id': this.product.imageUrl }
       },
+      
       'brandOwner': {
         '@type': 'Organization',
-        'organizationName': this.product.brandOwner.name
+        'organizationName': this.product.brandOwner.name,
+        'gln': this.product.brandOwner.gln,
+        'address': {
+          '@type': 'PostalAddress',
+          'streetAddress': this.product.brandOwner.streetAddress,
+          'postalCode': this.product.brandOwner.postalCode,
+          'addressLocality': this.product.brandOwner.city,
+          'addressRegion': this.product.brandOwner.province,
+          'addressCountry': {
+            '@type': 'Country',
+            'countryCode': this.product.brandOwner.countryCode
+          }
+        },
+        'telephone': this.product.brandOwner.telephone,
+        'homepage': {
+          '@id': 'https://' + this.product.brandOwner.website
+        }
       },
+
       'netWeight': {
         '@type': 'QuantitativeValue',
         'value': { '@value': this.product.netWeight, '@type': 'xsd:float' },
@@ -183,7 +210,24 @@ export class ProductComponent implements OnInit {
         'value': { '@value': this.product.dimensions.depth, '@type': 'xsd:float' },
         'unitCode': 'MMT'
       },
+      
       'ingredientStatement': [{ '@value': this.product.ingredients, '@language': 'it' }],
+      
+      'ingredient': [
+        {
+          '@type': 'FoodBeverageTobaccoIngredientDetails',
+          'ingredientName': [{ '@value': 'Fragole', '@language': 'it' }],
+          'ingredientContentPercentage': {
+            '@type': 'QuantitativeValue',
+            'value': { '@value': '70.0', '@type': 'xsd:float' },
+            'unitCode': 'P1'
+          }
+        },
+        { '@type': 'FoodBeverageTobaccoIngredientDetails', 'ingredientName': [{ '@value': 'Acqua', '@language': 'it' }] },
+        { '@type': 'FoodBeverageTobaccoIngredientDetails', 'ingredientName': [{ '@value': 'Zucchero', '@language': 'it' }] },
+        { '@type': 'FoodBeverageTobaccoIngredientDetails', 'ingredientName': [{ '@value': 'Gelificante: pectina da frutta', '@language': 'it' }] }
+      ],
+
       'consumerStorageInstructions': [{ '@value': this.product.storageInstructions, '@language': 'it' }],
       
       'nutrientBasisQuantity': {
